7845fb582b0f63cb6c02e5a4f1a1780812fcb3a2
AudriusAnusauskas <aaaudrius@gmail.com>
2023-05-26 21:04:56 +0300

    finish task 6 module 3 stage 0

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        float decimalPart = numberToBeRounded - (int) numberToBeRounded;
+        int result = 0;
+        if (decimalPart < 0.5) {
+            result = (int) numberToBeRounded;
+        } else if (decimalPart >= 0.5) {
+            result = (int) numberToBeRounded + 1;
+        }
+        System.out.println(result);
     }
 }
